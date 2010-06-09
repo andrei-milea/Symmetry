73accb4a3b850f31b08f70718e355064db78d8a4
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-06-09 16:39:09 +0000

    added lambda

--- gcc/gen_rep.h
+++ gcc/gen_rep.h
@@ -1,7 +1,11 @@
 #ifndef GEN_REP_H_
 #define GEN_REP_H_
 
-#include <set>
+#include <tr1/unordered_set>
+#include <algorithm>
+#include <iostream>
+
+namespace tr1 = std::tr1;
 
 //general group representation class
 template <typename T>
@@ -11,10 +15,10 @@ public:
 	cGenRep(std::size_t size)
 		:m_GroupOrder(size)
 	{
-		m_Set = new std::set<T>(size);
+		//m_Set = new std::set<T>(size);
 	};
 
-	cGenRep(std::set<T> &set)
+	cGenRep(tr1::unordered_set<T> &set)
 		:m_GroupOrder(set.size())
 	{
 		m_Set = set;
@@ -22,16 +26,33 @@ public:
 
 	~cGenRep()	{};
 
-	std::set<T>& GetGroupSet()const
+	tr1::unordered_set<T>& GetGroupSet()const
 	{
 		return m_Set;
 	};
+
+	void PrintGroupTable()const
+	{
+		std::for_each(m_Set.begin(), m_Set.end(), 
+				[&m_Set] (typename tr1::unordered_set<T>::const_iterator it)
+				{
+					std::cout<< "Table for "<< (*it) << ": \n";	
+					std::for_each(m_Set.begin(), m_Set.end(), 
+						[&it](typename tr1::unordered_set<T>::const_iterator it1)
+						{std::cout<< T::BinOpType::BinOp(*(it),*(it1)) << "\n"; });
+				});
+	};
+
+	/*void PrintElementTable(tr1::unordered_set<T>::const_iterator it)const
+	{
+		std::for_each(m_Set.begin(), m_Set.end(), [&m_Set] (it))
+	};*/
 
 
 	
 
 private:
-	std::set<T> m_Set;
+	tr1::unordered_set<T> m_Set;
 	std::size_t m_GroupOrder;
 };
 
