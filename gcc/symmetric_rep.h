5a995720adaf9d83e88cd895e9fbd1e618aaa7c3
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-06-22 19:19:39 +0000

    permutation multiplication broken

--- gcc/symmetric_rep.h
+++ gcc/symmetric_rep.h
@@ -10,28 +10,32 @@
 
 //symmertric group internal representation
 //used only from cGroup
-template <typrename T>
+template <typename T, std::size_t ORDER>
 class cSymmetricRep
 {
+public:
 	typename tr1::unordered_set<T>::iterator IteratorType;
+	typename cSymmetricRep<T, ORDER> SelfType;
+
 public:
 	//constructors
 	cSymmetricRep()
-		:m_GroupOrder(0)
 	{};
 
-	explicit cSymmetricRep(std::size_t group_order)
-		:m_GroupOrder(group_order)
-	{};
-	
 	cSymmetricRep(tr1::unordered_set<T> &generators_set)
-		:m_GroupOrder(generators_set.size()),
-		m_GeneratorsSet(generators_set)
+		:m_GeneratorsSet(generators_set)
 	{
 	};
 
 	//copy constructor and assign operator
-	
+	cSymmetricRep(const SelfType &sym_rep)		
+	{
+		m_GeneratorsSet = sym_rep.GetGeneratorsSet();
+	};
+	cSymmetricRep& operator=(const SelfType& sym_rep)
+	{
+		m_GeneratorsSet = sym_rep.GetGeneratorsSet();
+	};
 	
 
 	~cSymmetricRep()
@@ -40,16 +44,26 @@ public:
 
 	ListElementsNaive()
 	{
-
+		
 	};
 	ListElementsDimino()
 	{
 
 	};
 
+
+	//getter, setter
+	tr1::unorder_set<T> &GetGeneratorsSet()const
+	{
+		return m_GeneratorsSet;
+	};
+	void SetGeneratorsSet(tr1::unorder_set<T> &gen_set)
+	{
+		m_GeneratorsSet = gen_set;
+	};
+
 private:
 	tr1::unordered_set<T> m_GeneratorsSet;
-	std::size_t			  m_GroupOrder;
 };
 
 #endif
