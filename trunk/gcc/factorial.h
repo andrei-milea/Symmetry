b85f6f2357dcd2d181f14d84a7a154970f4fc5f6
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-06-30 20:23:03 +0000

    added compile time factorial

---
+++ trunk/gcc/factorial.h
@@ -0,0 +1,26 @@
+#ifndef FACTORIAL_H_
+#define FACTORIAL_H_
+
+
+//compute factorial at compile time
+//used for group size 
+//maximum size -- 17
+//TODO increase the alowed size [array of const]
+
+
+template <std::size_t VAL>
+struct Factorial 
+{
+	enum { value = VAL * Factorial<VAL - 1>::value };
+};
+ 
+template <>
+struct Factorial<0> 
+{
+	enum { value = 1 };
+};
+
+
+#endif
+
+
