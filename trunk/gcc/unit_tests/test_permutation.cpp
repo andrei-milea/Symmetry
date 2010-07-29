5f3bcbb467b25e08367fc977598f4fd91d2252a0
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-07-29 19:45:03 +0000

    test_group_el sketch added

--- trunk/gcc/unit_tests/test_permutation.cpp
+++ trunk/gcc/unit_tests/test_permutation.cpp
@@ -77,6 +77,9 @@ void test_perm_trivial()
     BOOST_CHECK(cPermElem<3>::GetIdentity() == perm1);
 }
 
+
+
+
 test_suite* init_unit_test_suite( int argc, char* argv[] ) 
 {
     framework::master_test_suite().add( BOOST_TEST_CASE( &test_perm_unity ) );
