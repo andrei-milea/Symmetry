b5aec34cd34314fc23766dee145ac8bb486f6681
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-10-31 22:17:49 +0000

    test case permutation D8 && fixes to cyclic extension method

--- gcc/tst_group.cpp
+++ gcc/tst_group.cpp
@@ -19,36 +19,9 @@ int main()
 	generators.push_back(elem2);
 	S3 g1(generators);
 
-	std::vector< cGroupElem< cPermElem, Multiplication> > normalizer_el;
-
-	cPermElem a1(3,{1,2,3});
-	cPermElem a2(3,{1,3,2});
-	cGroupElem< cPermElem, Multiplication> elem_a1(a1);
-	std::cout<<"sizeof"<<sizeof(elem_a1);
-
-//	cGroupElem< cPermElem, Multiplication> elem_a2(a2);
-//	std::vector< cGroupElem< cPermElem, Multiplication> > a3_el;
-//
-//	a3_el.push_back(g1.GetIdentity());
-//	a3_el.push_back(elem_a1);
-//	a3_el.push_back(elem_a2);
-//	S3 a3(a3_el);
-//	cSubgroup<S3> A3(a3_el);
-//	normalizer_el = g1.GetNormalizerEl(A3);
-//
-//	std_ex::print_vector(normalizer_el);
-
-//	//?????? -- not sure
-//	generators.clear();
-//	generators.push_back(g1.GetIdentity());
-//	if(!std_ex::set_equality(normalizer_el, generators));
-//	std::cout<<"error\n";
-
-//	std::vector< cGroupElem< cPermElem, Multiplication> > iden_el;
-//	iden_el.push_back(g1.GetIdentity());
-//	cSubgroup<S3> iden(iden_el);
-//	S3 g2(g1.GetNormalizer(iden));
-//	BOOST_CHECK(std_ex::set_equality(g1.GetElementsDimino(), g2.GetElementsDimino()));
+	cGrpLattice<S3> s3_lattice(g1);
+	s3_lattice.CyclicExtensionMethod();
+	std::cout<<s3_lattice;
 
 	return 0;	
 };
