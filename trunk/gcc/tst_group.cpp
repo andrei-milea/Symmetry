e91fc31d26a0f2b14ee2530de246abdd276f2ce2
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-11-07 10:35:29 +0000

    lattice reloaded

--- trunk/gcc/tst_group.cpp
+++ trunk/gcc/tst_group.cpp
@@ -10,30 +10,30 @@
 int main()
 {
 
-	cPermElem s1(3,{1,2});
-	cPermElem s2(3,{2,3});
-	cGroupElem< cPermElem, Multiplication> elem1(s1);
-	cGroupElem< cPermElem, Multiplication> elem2(s2);
-	std::vector< cGroupElem<cPermElem, Multiplication> >  generators;
-	generators.push_back(elem1);
-	generators.push_back(elem2);
-	S3 g1(generators);
+//	cPermElem s1(3,{1,2});
+//	cPermElem s2(3,{2,3});
+//	cGroupElem< cPermElem, Multiplication> elem1(s1);
+//	cGroupElem< cPermElem, Multiplication> elem2(s2);
+//	std::vector< cGroupElem<cPermElem, Multiplication> >  generators;
+//	generators.push_back(elem1);
+//	generators.push_back(elem2);
+//	S3 g1(generators);
+//
+//	cGrpLattice<S3> s3_lattice(g1);
+//	s3_lattice.CyclicExtensionMethod();
+//	std::cout<<s3_lattice;
 
-	cGrpLattice<S3> s3_lattice(g1);
-	s3_lattice.CyclicExtensionMethod();
-	std::cout<<s3_lattice;
 
-//
-//	cGroupElem<cPermElem, Multiplication> elt2({4,1,2,3});
-//	cGroupElem<cPermElem, Multiplication> elt5({1,4,3,2});
-//	cGroup<cGroupElem<cPermElem, Multiplication>, cSymmetricRep> D8;
-//	D8.AddGenerator(elt2);
-//	D8.AddGenerator(elt5);
-//
-//	cGrpLattice<cGroup<cGroupElem<cPermElem,Multiplication>, cSymmetricRep> > D8_lat(D8);
-//	D8_lat.CyclicExtensionMethod();
-//	std::cout<<D8_lat;
-//
+	cGroupElem<cPermElem, Multiplication> elt2({4,1,2,3});
+	cGroupElem<cPermElem, Multiplication> elt5({1,4,3,2});
+	cGroup<cGroupElem<cPermElem, Multiplication>, cSymmetricRep> D8;
+	D8.AddGenerator(elt2);
+	D8.AddGenerator(elt5);
+
+	cGrpLattice<cGroup<cGroupElem<cPermElem,Multiplication>, cSymmetricRep> > D8_lat(D8);
+	D8_lat.CyclicExtensionMethod();
+	std::cout<<D8_lat;
+
 	return 0;	
 };
 
