7c8814bdcfa2b3c61a6ea9a7aab478e6eebeb831
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2010-12-30 08:08:57 +0000

    added interface to gcc solution

---
+++ trunk/gcc/interface/http_server/http_request.h
@@ -0,0 +1,59 @@
+#ifndef _HTTP_REQUEST
+#define _HTTP_REQUEST
+
+#include <string>
+#include "http_header.h"
+
+
+#include <boost/spirit/include/support_multi_pass.hpp>
+
+enum REQ_METHOD
+{
+    INDETERMINATE_M = 0,
+    GET_M,
+    POST_M,
+    HEAD_M,
+    PUT_M,
+    OPTIONS_M,
+    DELETE_M,
+    TRACE_M,
+    CONNECT_M
+};
+
+
+
+class cRequest
+{
+    typedef std::istreambuf_iterator<char> base_iterator_type;
+    typedef boost::spirit::multi_pass<base_iterator_type> forward_iterator_type;
+
+public:
+    cRequest(std::istream &stream);
+    ~cRequest() {};
+
+    void ParseRequest();
+    std::vector<cHeader*> ParseHeaders();
+    void ParseBody();
+
+    REQ_METHOD            GetMethod()const
+    {   return m_Method;    };
+    const std::string&    GetResource()const
+    {   return m_Resource;  };
+    const std::string&    GetVersion()const
+    {   return m_Version;   };
+
+private:
+    REQ_METHOD                      m_Method;
+    std::string                     m_Resource;
+    std::string                     m_Version;
+    std::string                     m_Headers;
+
+    //stream iterators
+    forward_iterator_type           m_Fwd_begin;
+    forward_iterator_type           m_Fwd_end;
+
+};
+
+
+
+#endif
---
+++ trunk/gcc/interface/http_server/http_server.cpp
@@ -0,0 +1,29 @@
+
+#include "http_server.h"
+#include <boost/bind.hpp>
+
+
+using namespace boost::asio::ip;
+
+cHttpServer::cHttpServer(boost::asio::io_service& io_service)
+    :m_Acceptor(io_service, tcp::endpoint(tcp::v4(), 80))
+{
+    StartAccept();
+};
+
+void cHttpServer::StartAccept()
+{
+    cHttpConnection::pointer new_connection = cHttpConnection::Create(m_Acceptor.io_service());
+
+    m_Acceptor.async_accept(new_connection->GetSocket(),
+        boost::bind(&cHttpServer::HandleConnection, this, new_connection, boost::asio::placeholders::error));
+};
+
+void cHttpServer::HandleConnection(cHttpConnection::pointer new_connection, const boost::system::error_code& error)
+{
+    if (!error)
+    {
+        new_connection->HandleClient();
+        StartAccept();
+    }
+};
