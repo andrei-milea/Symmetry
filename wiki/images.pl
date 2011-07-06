5533634a666d1e7cf1bbbd37a92396bfdfd4b641
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2011-07-06 23:00:54 +0000

    added wiki

---
+++ wiki/images.pl
@@ -0,0 +1,18 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/1mapsto2;2mapsto1;3mapsto3;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="154" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$1\mapsto2;2\mapsto1;3\mapsto3$">|; 
+
+$key = q/1mapsto1;2mapsto2;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="101" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$1\mapsto1;2\mapsto2$">|; 
+
+1;
+
