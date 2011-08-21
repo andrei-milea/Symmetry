085c3f52044983dec4c89f4ad7bf47eb701b8065
biotech86 <biotech86@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2011-08-21 14:11:04 +0000

    fixed command parsing + web dev

--- trunk/gcc/interface/pages/command_panel.js
+++ trunk/gcc/interface/pages/command_panel.js
@@ -1,8 +1,10 @@
+
 //command panel object -- used manipulate the command panel div
 var Command_panel = {
-    state_main_comm: "hidden",
+    state_main_comm: "visible",
     state_canvas: "hidden",
 	state_groups: "hidden",
+	state_create_grp: "hidden",
     
     toggle_panel: function()
     {
@@ -12,6 +14,49 @@ var Command_panel = {
             this.show_panel();
             
     },
+
+    show_panel: function() 
+    {
+        var togg_button = document.getElementById("toggle_button");
+        togg_button.setAttribute("value", "Hide Commands Panel");
+        var command_panelDiv = document.getElementById("command_panel");
+		command_panelDiv.style.display = "block";
+        this.state_main_comm = "visible";
+    },
+    
+    hide_panel: function() 
+    {
+        var togg_button = document.getElementById("toggle_button");
+        togg_button.setAttribute("value", "Show Commands Panel");
+        var command_panelDiv = document.getElementById("command_panel");
+		command_panelDiv.style.display = "none";
+        this.state_main_comm = "hidden";
+    },
+
+	show_general_commands_panel: function()
+	{
+		var generalcommand_panelDiv = document.getElementById("general_commands_panel");
+		generalcommand_panelDiv.style.display = "block";
+	},
+
+	hide_general_commands_panel: function()
+	{
+        var generalcommand_panelDiv = document.getElementById("general_commands_panel");
+		generalcommand_panelDiv.style.display = "none";
+	},
+
+	show_group_commands_panel: function()
+	{
+        var groupcommand_panelDiv = document.getElementById("group_commands_panel");
+		groupcommand_panelDiv.style.display = "block";
+	},
+
+	hide_group_commands_panel: function()
+	{
+        var groupcommand_panelDiv = document.getElementById("group_commands_panel");
+		groupcommand_panelDiv.style.display = "none";
+	},
+ 
     
     toggle_canvas: function()
     {
@@ -32,153 +77,235 @@ var Command_panel = {
         html_body.removeChild(main_viewTag);
         
         var draw_canvasTag = document.createElement("canvas");
-        draw_canvasTag.setAttribute("id", "draw_canvas");
+        draw_canvasTag.setAttribute("id", "draw_canvas_id");
         draw_canvasTag.setAttribute("class", "scanvas");  
-        html_body.appendChild(draw_canvasTag);
+
+
+        var finalDiv = document.getElementById("final");
+        html_body.insertBefore(draw_canvasTag, finalDiv);
     },
     
     show_main_view: function()
     {
         var canvas_button = document.getElementById("canvas_button");
-        canvas_button.setAttribute("value", "Switch to visualization mode");
+        canvas_button.setAttribute("value", "Switch to visual mode");
         this.state_canvas = "hidden";
         
-        var draw_canvasTag = document.getElementById("draw_canvas");
+        var draw_canvasTag = document.getElementById("draw_canvas_id");
         var html_body = document.getElementById("body_id");
         html_body.removeChild(draw_canvasTag);
         
         var main_viewTag = document.createElement("div");
         main_viewTag.setAttribute("id", "main_view_id");
         main_viewTag.setAttribute("class", "smain_view");  
-        html_body.appendChild(main_viewTag);   
-        
-    },
-    
-    
-    show_panel: function() 
-    {
-        var togg_button = document.getElementById("toggle_button");
-        togg_button.setAttribute("value", "Hide Commands Panel");
-        this.state_main_comm = "visible";
-        this.add_commands();
-    },
-    
-    hide_panel: function() 
-    {
-        var togg_button = document.getElementById("toggle_button");
-        togg_button.setAttribute("value", "Show Commands Panel");
-        this.state_main_comm = "hidden";
-        this.remove_commands();
-    },
-    
-    add_commands: function()
-    {
-        var command_panelDiv = document.getElementById("command_panel");
-        var command_selectTag = document.createElement("a");
-        var command_createTag = document.createElement("a");
-        var brTag1 = document.createElement("br");
-        brTag1.setAttribute("id", "br1");
-        var brTag2 = document.createElement("br");
-        brTag2.setAttribute("id", "br2");
-        
-        command_selectTag.setAttribute("id", "command_select");
-        command_selectTag.setAttribute("href", "#");
-        command_selectTag.setAttribute("onmouseover", "cTooltip.show('select');");
-        command_selectTag.setAttribute("onmouseout", "cTooltip.hide();");
-        command_selectTag.setAttribute("onclick", "Command_panel.add_groups()");
-        command_selectTag.innerHTML = "Select Group";
-        
-        command_createTag.setAttribute("id", "command_create");
-        command_createTag.setAttribute("href", "#");
-        command_createTag.setAttribute("onmouseover", "cTooltip.show('create');");
-        command_createTag.setAttribute("onmouseout", "cTooltip.hide();");
-        command_createTag.innerHTML = "Create Group";
-        
-        command_panelDiv.appendChild(brTag1);
-        command_panelDiv.appendChild(command_selectTag);
-        command_panelDiv.appendChild(brTag2);
-        command_panelDiv.appendChild(command_createTag);
-    },
-    
-    remove_commands: function()
-    {
-        var command_panelDiv = document.getElementById("command_panel");
-        var brTag1 = document.getElementById("br1");
-        var brTag2 = document.getElementById("br2");
-        var command_selectTag = document.getElementById("command_select");
-        var command_createTag = document.getElementById("command_create");
-        
-        command_panelDiv.removeChild(brTag1);
-        command_panelDiv.removeChild(brTag2);
-        
-        command_panelDiv.removeChild(command_selectTag);
-        command_panelDiv.removeChild(command_createTag);
-    },
 
+        var finalDiv = document.getElementById("final");
+        html_body.insertBefore(main_viewTag, finalDiv);   
+        
+    },
+    
 	add_groups: function()
     {
-        var command_panelDiv = document.getElementById("command_panel");
-        var command_createTag = document.getElementById("command_create");
+		if(this.state_groups === "hidden")
+		{
+			var command_panelDiv = document.getElementById("general_commands_panel");
+			var command_createTag = document.getElementById("command_create");
 
-        var s3Tag = document.createElement("a");
-        var d8Tag = document.createElement("a");
-        var brTag4 = document.createElement("br");
-        brTag4.setAttribute("id", "br4");
-		var brTag5 = document.createElement("br");
-        brTag5.setAttribute("id", "br5");
+			var s3Tag = document.createElement("a");
+			var d8Tag = document.createElement("a");
+			var brTag4 = document.createElement("br");
+			brTag4.setAttribute("id", "br4");
+			var brTag5 = document.createElement("br");
+			brTag5.setAttribute("id", "br5");
 
 
 
-        s3Tag.setAttribute("id", "s3");
-        s3Tag.setAttribute("href", "#");
-        s3Tag.setAttribute("onmouseover", "cTooltip.show('s3');");
-        s3Tag.setAttribute("onmouseout", "cTooltip.hide();");
-        s3Tag.innerHTML = "S3";
-        
-        d8Tag.setAttribute("id", "d8");
-        d8Tag.setAttribute("href", "#");
-        d8Tag.setAttribute("onmouseover", "cTooltip.show('d8');");
-        d8Tag.setAttribute("onmouseout", "cTooltip.hide();");
-        d8Tag.innerHTML = "D8";
-        
-        command_panelDiv.insertBefore(s3Tag, command_createTag);
-        command_panelDiv.insertBefore(brTag4, command_createTag);
-        command_panelDiv.insertBefore(d8Tag, command_createTag);
-        command_panelDiv.insertBefore(brTag5, command_createTag);
+			s3Tag.setAttribute("id", "s3");
+			s3Tag.setAttribute("href", "#");
+			s3Tag.setAttribute("onmouseover", "cTooltip.show('s3');");
+			s3Tag.setAttribute("onmouseout", "cTooltip.hide();");
+			s3Tag.innerHTML = "S3";
+			
+			d8Tag.setAttribute("id", "d8");
+			d8Tag.setAttribute("href", "#");
+			d8Tag.setAttribute("onmouseover", "cTooltip.show('d8');");
+			d8Tag.setAttribute("onmouseout", "cTooltip.hide();");
+			d8Tag.innerHTML = "D8";
+			
+			command_panelDiv.insertBefore(s3Tag, command_createTag);
+			command_panelDiv.insertBefore(brTag4, command_createTag);
+			command_panelDiv.insertBefore(d8Tag, command_createTag);
+			command_panelDiv.insertBefore(brTag5, command_createTag);
+			this.state_groups = "visible";
+		}
+		else
+		{
+			this.remove_groups();
+		}
     },
 
 	remove_groups: function()
     {
-        var command_panelDiv = document.getElementById("command_panel");
+        var command_panelDiv = document.getElementById("general_commands_panel");
         var brTag4 = document.getElementById("br4");
         var brTag5 = document.getElementById("br5");
         var s3Tag = document.getElementById("s3");
-        var s3Tag = document.getElementById("d8");
+        var d8Tag = document.getElementById("d8");
         
         command_panelDiv.removeChild(brTag4);
         command_panelDiv.removeChild(brTag5);
         
         command_panelDiv.removeChild(s3Tag);
         command_panelDiv.removeChild(d8Tag);
+		this.state_groups = "hidden";
     },
 
+	add_create_grp_div: function()
+	{	
+		if(this.state_create_grp === "hidden")
+		{
+			var command_panelDiv = document.getElementById("general_commands_panel");
 
+			var createGrpDivTag = document.createElement("div");
+			createGrpDivTag.setAttribute("id", "grp_div_id");
+			createGrpDivTag.setAttribute("onmouseover", "cTooltip.show('generator')");
+			createGrpDivTag.setAttribute("onmouseout", "cTooltip.hide()");
+			createGrpDivTag.innerHTML="</br>&nbsp;&nbsp;&nbsp;<form onSubmit='Command_panel.submit_grp()' method='get'>Enter generator: &nbsp;&nbsp;&nbsp;<input type='text' id='generator_id'>&nbsp;&nbsp;&nbsp;<input type='button' value='Add' onclick='Command_panel.add_generator()'>&nbsp;&nbsp;&nbsp;<select id='grp_sel_id' name='Group Type'> <option>Symmetric Group</option></select></br></br>Generators:</br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<select id='generators_id' size='5' style='Width:200' ></select>&nbsp;&nbsp;&nbsp;<input type='submit' value='Go'></form></br></br>";
+			command_panelDiv.appendChild(createGrpDivTag);
+			this.state_create_grp = "visible";
+		}
+		else
+		{
+			this.remove_create_grp_div();
+		}
+
+	},
+
+	remove_create_grp_div: function()
+   {
+		var command_panelDiv = document.getElementById("general_commands_panel");
+		grp_divTag = document.getElementById("grp_div_id");
+		command_panelDiv.removeChild(grp_divTag);
+		this.state_create_grp = "hidden";
+   },
+
+	add_generator: function()
+	{
+		var generatorTag = document.getElementById("generator_id");
+		var generatorsTag = document.getElementById("generators_id");
+		var newgenTag = document.createElement("option");
+		newgenTag.innerHTML = generatorTag.value;
+		generatorsTag.appendChild(newgenTag);
+	},
+
+	static_vars: function()
+	{},
+
+	submit_grp: function()
+	{
+		var generators = new String();
+		var generatorsTag = document.getElementById("generators_id");
+		for(var geni = 0; geni < generatorsTag.length; geni++)
+		{
+			 generators += generatorsTag.options[geni].text;
+		}
+		this.static_vars.generators = generators;
+		var group_selTag = document.getElementById("grp_sel_id");
+		this.static_vars.group_type = group_selTag.options[group_selTag.selectedIndex].value;
+
+		this.hide_general_commands_panel();
+		this.show_group_commands_panel();
+
+		//add to the main div
+		var command_panelDiv = document.getElementById("command_panel");
+		var group_propertyDiv = document.createElement("div");
+		command_panelDiv.appendChild(group_propertyDiv);
+		group_propertyDiv.setAttribute("id", "group_property_div_id");
+		property_tableTag = document.createElement("table");
+		property_tableTag.setAttribute("id", "grp_table_id");
+		property_tableTag.setAttribute("border", "1");
+		group_propertyDiv.appendChild(document.createElement("br"));
+		group_propertyDiv.appendChild(property_tableTag);
+
+		this.add_group_property("Group Type", this.static_vars.group_type);
+		this.add_group_property("Group Generators", this.static_vars.generators);
+	},
+
+	add_group_property: function(property, value)
+	{
+		var property_tableTag = document.getElementById("grp_table_id")
+		rowTag = document.createElement("tr");
+		cell1Tag = document.createElement("td");
+		cell2Tag = document.createElement("td");
+		content1Tag = document.createTextNode(property);
+		content2Tag = document.createTextNode(value);
+		cell1Tag.appendChild(content1Tag);
+		cell2Tag.appendChild(content2Tag);
+		rowTag.appendChild(cell1Tag);
+		rowTag.appendChild(cell2Tag);
+		property_tableTag.appendChild(rowTag);
+	},
+
+	goto_general_panel: function()
+	{
+		this.show_general_commands_panel();
+		this.hide_group_commands_panel();
+		var group_propertyDiv = document.getElementById("group_property_div_id");
+		var command_panelDiv = document.getElementById("command_panel");
+		command_panelDiv.removeChild(group_propertyDiv);
+	},
+
+	submit_grp_command: function(command)
+	{
+
+		var group_types_map = new Object();
+		group_types_map["Symmetric Group"] = "SYMMETRIC_GROUP";
+
+		var request = new String();
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		{// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		}
+		else
+		{// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		} 
+
+		var sessionkeyTag = document.getElementById("session_key_id");
+
+		request = "id=" + sessionkeyTag.value;
+		request += "command=" + command;
+		request += "param=" + group_types_map[this.static_vars.group_type];
+		request	+= this.static_vars.generators;
+		alert(request);
+		xmlhttp.open("GET", request, false);
+		xmlhttp.send();
+		document.getElementById("main_view_id").innerHTML=xmlhttp.responseText;
+	}
 	
 };
 
 
 
+
+
 var cTooltip=function(){
-	var endalpha = 95;
+	var endalpha = 70;
 	var alpha = 0;
 	var timer = 20;
 	var speed = 10;
 	var tooltip;
 	var tooltip_map = new Object();
-	tooltip_map.select = "Select a predefined group";
-	tooltip_map.create = "Create a custom group by specifying type and generators";
-	tooltip_map.s3 = "Symmetric group of order 3";
-	tooltip_map.d8 = "Dihedral group of order 8";
+	tooltip_map.select = "Select a predefined group.";
+	tooltip_map.create = "Create a custom group by specifying type and generators.";
+	tooltip_map.s3 = "Symmetric group of order 3.";
+	tooltip_map.d8 = "Dihedral group of order 8.";
+	tooltip_map.generator = "Generators are of the form:</br> 1.For permutation group -- </br> 2.For matrix groups -- </br> 3.For polynomial groups -- .";
+	tooltip_map.tutorials = "Visit our tutorial videos on our Youtube page.";
+	tooltip_map.source = "Visit the project repository on Google Code.";
+	tooltip_map.get_elem = "Compute group elements using Dimono algorithm(permutation groups)."
+	tooltip_map.prev_panel = "Go back to the previous panel (cancel ongoing action)."
 
 return{	
 	show: function(callee)
@@ -234,18 +361,5 @@ return{
 };
 }(); 
 
-
-var xmlhttp;
- if (window.XMLHttpRequest)
-   {// code for IE7+, Firefox, Chrome, Opera, Safari
-   xmlhttp=new XMLHttpRequest();
-   }
- else
-   {// code for IE6, IE5
-   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-   } 
-   xmlhttp.open("GET","ajax_info.txt",false);
- 	xmlhttp.send();
- 	document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
 
     
