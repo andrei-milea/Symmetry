266ac02ca4104432c0e2fe56644e3703a4b2f49f
milea.andrei <milea.andrei@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2012-02-09 20:44:38 +0000

    removed doc

--- gcc/engine/doc/html/classengine_1_1cGetElemCommand.js
+++
@@ -1,7 +0,0 @@
-var classengine_1_1cGetElemCommand =
-[
-    [ "cGetElemCommand", "classengine_1_1cGetElemCommand.html#ae65189d2ef82b1ca49295268846b1dd6", null ],
-    [ "~cGetElemCommand", "classengine_1_1cGetElemCommand.html#a409827fef83a7bdcdddba9f2f130a0dd", null ],
-    [ "EstimateRunTime", "classengine_1_1cGetElemCommand.html#ad84c73fe5b4db65679f28c427d201434", null ],
-    [ "Execute", "classengine_1_1cGetElemCommand.html#a16a627c20d55b3f2538bab7f63a04a04", null ]
-];
--- gcc/lib/doc/html/classcGroupRelation.js
+++
@@ -1,8 +0,0 @@
-var classcGroupRelation =
-[
-    [ "cGroupRelation", "classcGroupRelation.html#a42b75d7cdd24d87b271f067cbcede66b", null ],
-    [ "~cGroupRelation", "classcGroupRelation.html#a19e65ad20ad16d7f3aa3d1ad6307caaf", null ],
-    [ "AddElement", "classcGroupRelation.html#aeae9fe8f02b40a3cf1b7f73abee2b9cd", null ],
-    [ "Simplify", "classcGroupRelation.html#ac93e1c0794cbca8e574cbf1c4255c675", null ],
-    [ "m_Elements", "classcGroupRelation.html#a71fe79690808fb895d27eee80edc99d4", null ]
-];
