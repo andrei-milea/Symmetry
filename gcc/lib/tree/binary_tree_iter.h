cfbe2a49fe8fdfc3dccb24aaf70cd0e8dfa8aa54
milea.andrei <milea.andrei@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2012-03-11 22:05:54 +0000

    finished binary_rep ; started threaded rep

--- gcc/lib/tree/binary_tree_iter.h
+++ gcc/lib/tree/binary_tree_iter.h
@@ -12,88 +12,331 @@ namespace tree
 {
 
 /*!
-  binary tree iterator class 
+  binary tree iterator base class 
   uses boost::iterator_facade to implement the iterator
-  the tree_traversal order must be specified in the constructor
 */
 template <typename T, typename N>
 class btree_iterator : public boost::iterator_facade<btree_iterator<T, N>, T, boost::forward_traversal_tag>
 {
-public:
-	btree_iterator(tree_traversal traversal = INORDER)
-		:m_Current(nullptr),
-		m_NextNode(nullptr),
-		m_Traversal(traversal)
+protected:
+	btree_iterator()
+		:m_Current(nullptr)
 	{};
-	explicit btree_iterator(N* node, tree_traversal traversal = INORDER)
-		:m_Current(node),
-		m_NextNode(node),
-		m_Traversal(traversal)
+	explicit btree_iterator(N* node)
+		:m_Current(node)
 	{};
 
-private:
     friend class boost::iterator_core_access;
-
-	void increment()
-	{
-		if(nullptr == m_Current)
-			throw std::out_of_range("btree_iterator.increment -- invalid current node");
-		if(INORDER == m_Traversal)
-		{
-			while(nullptr != m_NextNode)
-			{
-				m_Stack.push_back(m_NextNode);
-				m_NextNode = m_NextNode->left;
-			}
-			if(!m_Stack.empty())
-			{
-				m_NextNode = m_Stack.pop_back();
-				m_Current = m_NextNode;
-				m_NextNode = m_NextNode->right;
-			}
-			else
-				m_Current = nullptr;
-		}
-		else if(PREORDER == m_Traversal)
-		{
-			if(m_NextNode == m_Current)
-				m_Stack.push_back(m_NextNode);
-			if(!m_Stack.empty())
-			{
-				m_NextNode = m_Stack.pop_back();
-				if(nullptr != m_NextNode->right)
-					m_Stack.push_back(m_NextNode->right);
-				if(nullptr != m_NextNode->left)
-					m_Stack.push_back(m_NextNode->left);
-				m_Current = m_NextNode;
-			}
-			else
-			{
-				m_Current = nullptr;
-			}
-		}
-		else
-		{
-			assert(POSTORDER == m_Traversal);
-		}
-	};
+	template <typename> friend class cBinaryRep;
 
 	bool equal(const btree_iterator<T, N>& other)const
 	{
-		return this->m_Current == other.m_Current;
+		return m_Current == other.m_Current;
 	};
 
     T& dereference()const
 	{
-		return m_Current->info;
+		return m_Current->data;
+	};
+	virtual void increment()
+	{
+	};
+
+protected:
+	N *m_Current;
+};
+
+
+/*!
+  traverses the tree in preorder(root, left, right)
+  using a stack
+*/
+template <typename T, typename N>
+class btree_preorder_iterator : public btree_iterator<T, N> 
+{
+public:
+	btree_preorder_iterator()
+		:btree_iterator<T, N>(),
+		m_NextNode(this->m_Current)
+	{};
+
+	explicit btree_preorder_iterator(N* node)
+		:btree_iterator<T, N>(node),
+		m_NextNode(this->m_Current)
+	{
+		m_Stack.push(m_NextNode);
+	};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+		if(!m_Stack.empty())
+		{
+			m_NextNode = m_Stack.top();
+			m_Stack.pop();
+			if(nullptr != m_NextNode->right)
+				m_Stack.push(m_NextNode->right);
+			if(nullptr != m_NextNode->left)
+				m_Stack.push(m_NextNode->left);
+			this->m_Current = m_NextNode;
+		}
+		else
+		{
+			this->m_Current = nullptr;
+		}
 	};
 
 private:
-	N *m_Current;
 	N *m_NextNode;
-	tree_traversal m_Traversal;
 	std::stack<N*> m_Stack;
+};
 
+/*!
+  traverses the tree in inorder(symmetric order -- left, root, right)
+  using a stack
+*/
+template <typename T, typename N>
+class btree_inorder_iterator : public btree_iterator<T, N> 
+{
+public:
+	btree_inorder_iterator()
+		:btree_iterator<T, N>(),
+		m_NextNode(this->m_Current)
+	{};
+	explicit btree_inorder_iterator(N* node)
+		:btree_iterator<T, N>(node),
+		m_NextNode(this->m_Current)
+	{};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+		while(nullptr != m_NextNode)
+		{
+			m_Stack.push(m_NextNode);
+			m_NextNode = m_NextNode->left;
+		}
+		if(!m_Stack.empty())
+		{
+			m_NextNode = m_Stack.top();
+			m_Stack.pop();
+			this->m_Current = m_NextNode;
+			m_NextNode = m_NextNode->right;
+		}
+		else
+			this->m_Current = nullptr;
+	}
+
+private:
+	N *m_NextNode;
+	std::stack<N*> m_Stack;
+};
+
+/*!
+  traverses the tree in postorder(left, right, root)
+  using a stack
+*/
+template <typename T, typename N>
+class btree_postorder_iterator : public btree_iterator<T, N> 
+{
+public:
+	btree_postorder_iterator()
+		:btree_iterator<T, N>(),
+		m_NextNode(this->m_Current)
+	{};
+	explicit btree_postorder_iterator(N* node)
+		:btree_iterator<T, N>(node),
+		m_NextNode(this->m_Current)
+	{};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+		while(true)
+		{
+			if(nullptr != m_NextNode)
+			{
+				if(nullptr != m_NextNode->right)
+				{
+					m_Stack.push(m_NextNode->right);
+				}
+				m_Stack.push(m_NextNode);
+				m_NextNode = m_NextNode->left;
+				continue;
+			}
+
+			if(!m_Stack.empty())
+			{
+				m_NextNode = m_Stack.top();
+				m_Stack.pop();
+				if( (nullptr != m_NextNode->right) && !m_Stack.empty() && (m_Stack.top() == m_NextNode->right) )
+				{
+					m_Stack.pop();
+					m_Stack.push(m_NextNode);
+					m_NextNode = m_NextNode->right;
+				}
+				else
+				{
+					this->m_Current = m_NextNode;
+					m_NextNode = nullptr;
+					return;
+				}
+			}
+			else
+			{
+				this->m_Current = nullptr;
+				return;
+			}
+		}
+	}
+
+private:
+	N *m_NextNode;
+	std::stack<N*> m_Stack;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///partial specialization for iterators in a threaded binary tree representation///
+///////////////////////////////////////////////////////////////////////////////////
+
+/*!
+  binary tree iterator base class 
+  uses boost::iterator_facade to implement the iterator
+  !the threaded tree iterators are bidirectional
+*/
+template <typename T>
+class btree_iterator<T, btree_threaded_node<T> >: public boost::iterator_facade<btree_iterator<T, btree_threaded_node<T> >, T, boost::bidirectional_traversal_tag>
+{
+protected:
+	typedef btree_threaded_node<T> N;
+	btree_iterator()
+		:m_Current(nullptr)
+	{};
+	explicit btree_iterator(N* node)
+		:m_Current(node)
+	{};
+
+    friend class boost::iterator_core_access;
+	template <typename> friend class cThreadedRep;
+
+	bool equal(const btree_iterator<T, N>& other)const
+	{
+		return m_Current == other.m_Current;
+	};
+
+    T& dereference()const
+	{
+		return m_Current->data;
+	};
+	virtual void increment()
+	{};
+	virtual void decrement()
+	{};
+
+protected:
+	N *m_Current;
+};
+
+/*!
+  traverses the tree in preorder(root, left, right)
+  using a stack
+*/
+template <typename T>
+class btree_preorder_iterator<T, btree_threaded_node<T> >: public btree_iterator<T, btree_threaded_node<T> > 
+{
+	typedef btree_threaded_node<T> N;
+public:
+	btree_preorder_iterator()
+		:btree_iterator<T, N>()
+	{};
+
+	explicit btree_preorder_iterator(N* node)
+		:btree_iterator<T, N>(node)
+	{};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+	};
+	void decrement()
+	{
+		if(nullptr == this->m_Current)
+			return;
+	};
+};
+
+/*!
+  traverses the tree in inorder(symmetric order -- left, root, right)
+  using a stack
+*/
+template <typename T>
+class btree_inorder_iterator<T, btree_threaded_node<T> > : public btree_iterator<T, btree_threaded_node<T> > 
+{
+	typedef btree_threaded_node<T> N;
+public:
+	btree_inorder_iterator()
+		:btree_iterator<T, N>()
+	{};
+	explicit btree_inorder_iterator(N* node)
+		:btree_iterator<T, N>(node)
+	{};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+		if(true == this->m_Current->rTag)
+		{
+			this->m_Current = this->m_Current->right;
+			return;
+		}
+		else
+		{
+			this->m_Current = this->m_Current->right;
+		}
+		while(false == this->m_Current->lTag)
+			this->m_Current = this->m_Current->left;
+
+	};
+
+	void decrement()
+	{
+		if(nullptr == this->m_Current)
+			return;
+	};
+};
+
+/*!
+  traverses the tree in postorder(left, right, root)
+  using a stack
+*/
+template <typename T>
+class btree_postorder_iterator<T, btree_threaded_node<T> > : public btree_iterator<T, btree_threaded_node<T> > 
+{
+	typedef btree_threaded_node<T> N;
+public:
+	btree_postorder_iterator()
+		:btree_iterator<T, N>()
+	{};
+	explicit btree_postorder_iterator(N* node)
+		:btree_iterator<T, N>(node)
+	{};
+
+	void increment()
+	{
+		if(nullptr == this->m_Current)
+			return;
+	}
+	void decrement()
+	{
+		if(nullptr == this->m_Current)
+			return;
+	}
 };
 
 };//namespace tree
--- gcc/lib/tree/tree_common.h
+++ gcc/lib/tree/tree_common.h
@@ -27,15 +27,46 @@ struct btree_node
 		:left(nullptr),
 		right(nullptr)
 	{};
-	btree_node(T &_info)
-		:info(_info),
+	btree_node(const T &_data)
+		:data(_data),
 		left(nullptr),
 		right(nullptr)
 	{};
 
-	T info;
+	T data;
 	btree_node *left;
 	btree_node *right;
+};
+
+/*!
+  node type used by the binary tree threaded representation
+  rTag = false -> right points to child
+  rTag = true -> right points to the inorder predecesor
+  lTag = false -> left points to a child 
+  lTag = true -> left points to the inorder successor
+*/
+template <typename T>
+struct btree_threaded_node
+{
+	btree_threaded_node()
+		:left(nullptr),
+		right(nullptr),
+		rTag(0),
+		lTag(0)
+	{};
+	btree_threaded_node(const T &_data)
+		:data(_data),
+		left(nullptr),
+		right(nullptr),
+		rTag(0),
+		lTag(0)
+	{};
+
+	T data;
+	btree_threaded_node *left;
+	btree_threaded_node *right;
+	bool rTag;
+	bool lTag;
 };
 
 };
