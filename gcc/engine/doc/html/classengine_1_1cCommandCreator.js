da9a8243bd36368abbddcd6a6daee6b9aa0ca46e
milea.andrei <milea.andrei@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2012-12-24 18:31:48 +0000

    removed doc

--- gcc/engine/doc/html/classengine_1_1cCommandCreator.js
+++
@@ -1,5 +0,0 @@
-var classengine_1_1cCommandCreator =
-[
-    [ "GetCommand", "classengine_1_1cCommandCreator.html#afced57cc0c6330127f73929878031911", null ],
-    [ "isGroupCommand", "classengine_1_1cCommandCreator.html#a219016ab2021de8c15740a1392222766", null ]
-];
--- gcc/engine/doc/html/classengine_1_1cGetRelCommand.js
+++
@@ -1,9 +0,0 @@
-var classengine_1_1cGetRelCommand =
-[
-    [ "cGetRelCommand", "classengine_1_1cGetRelCommand.html#aa6962470ec3861855bab696e13cf8a63", null ],
-    [ "~cGetRelCommand", "classengine_1_1cGetRelCommand.html#a1f6830c8a09369f5296790dd73d0570c", null ],
-    [ "EstimateRunTime", "classengine_1_1cGetRelCommand.html#ad6aa9cb526ae1b73237edaad02d081ec", null ],
-    [ "Execute", "classengine_1_1cGetRelCommand.html#a77179357376646c2c4885652cd80b4d2", null ],
-    [ "GetResult", "classengine_1_1cGetRelCommand.html#a7cc00a86111fba4dceacf1213e05bc80", null ],
-    [ "m_Result", "classengine_1_1cGetRelCommand.html#a6d84c8a5636d079347ae0507fecba6cf", null ]
-];
