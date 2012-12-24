7543150f620fb7bdb8fc70da1f4b3720b2fa17ec
milea.andrei <milea.andrei@7b2255bc-459b-8015-f9de-a6ae923c82f0>
2012-12-24 18:51:00 +0000

    removed doc

--- trunk/gcc/engine/doc/html/search/functions_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cresultsbucket',['cResultsBucket',['../classresultsDB_1_1cResultsBucket.html#a959c37146bc088b2d8d69d51c63ae227',1,'resultsDB::cResultsBucket']]]
-];
--- trunk/gcc/lib/doc/html/search/functions_72.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rotateleft',['rotateLeft',['../classcPermElem.html#afa5694d1831e653f4cc3b12de6bb058e',1,'cPermElem']]]
-];
